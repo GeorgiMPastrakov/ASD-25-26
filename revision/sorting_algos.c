bd346db28e0aace1fb972958b4ed3f39f8c8f746
Georgi Pastrakov <160168282+GeorgiMPastrakov@users.noreply.github.com>
2025-09-23 23:40:28 +0300

    Add sorting algorithms implementation and header file

---
+++ revision/sorting_algos.c
@@ -0,0 +1,27 @@
+#include "sorting_algos.h"
+
+void printArr(int arr, uchar size){
+    for(uchar i = 0; i < size; i++){
+        
+    }
+}
+
+void bubbleSort(int arr, uchar size);
+
+void selectionSort(int arr, uchar size);
+
+void insertionSort(int arr, uchar size);
+
+void mergeSort(int arr, uchar size);
+
+void quickSort(int arr, uchar size);
+
+void heapSort(int arr, uchar size);
+
+void countingSort(int arr, uchar size);
+
+void radixSort(int arr, uchar size);
+
+void bucketSort(int arr, uchar size);
+
+void shellSort(int arr, uchar size);
---
+++ revision/sorting_algos.h
@@ -0,0 +1,21 @@
+#ifndef SORTING_ALGOS
+#define SORTING_ALGOS
+#include<stdio.h>
+
+#define SWAP(a, b) {a = a^b; b = a^b; a = a^b;}while (0)
+
+typedef unsigned char uchar;
+
+void printArr(int arr, uchar size);
+void bubbleSort(int arr, uchar size);
+void selectionSort(int arr, uchar size);
+void insertionSort(int arr, uchar size);
+void mergeSort(int arr, uchar size);
+void quickSort(int arr, uchar size);
+void heapSort(int arr, uchar size);
+void countingSort(int arr, uchar size);
+void radixSort(int arr, uchar size);
+void bucketSort(int arr, uchar size);
+void shellSort(int arr, uchar size);
+
+#endif
